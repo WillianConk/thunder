c3f24be980db9a5f988ac34467e1bd178e7f3c75
Willian Moro Conk <willmgrfu@gmail.com>
2017-09-10 14:49:59 -0300

    Devmind commit in 2017-09-10 14:51:40

--- porcoweb.php
+++ porcoweb.php
@@ -13,7 +13,9 @@ echo htmlentities("frango caiu na web, o porco tbm e o ilha é cotocooooooooooo"
 ?>
 
 <p><?php echo 'segue cotocagem sendo feita ainda' ; ?><p>
-<p><?php $name = 'arromba'; ?></p>
+<p><?php $name = 'arromba';
+echo $name 
+?></p>
 
 
 
