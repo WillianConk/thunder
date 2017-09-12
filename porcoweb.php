a74f093a314ed0f6c123b100f51022a36bfc7c45
Willian Moro Conk <willmgrfu@gmail.com>
2017-09-11 21:32:46 -0300

    Devmind commit in 2017-09-11 21:34:27

--- porcoweb.php
+++ porcoweb.php
@@ -3,20 +3,15 @@
 <body>
 
 <h1>porcoweb.caiunanet<h1>
-<h2> salva sozinho</h2>
-<h2> hue</h2>
+<h2> Salva sozinho? Sim</h2>
+<h2> php? sim</h2>
+<h3>teste</h3>
 <h3>jose</h3>
 <h3>josias</h3>
 <h3>josicreyton</h3>
 <?php
-echo htmlentities("frango caiu na web, o porco tbm e o ilha é cotocooooooooooo");
+echo htmlentities("eu sou o Messias");
 ?>
-
-<p><?php echo 'segue cotocagem sendo feita ainda' ; ?><p>
-<p><?php $name = 'fica todo arrombadinho';
-echo "oq acontece com seu rabo: " . $name ;
-?></p>
-
 
 
 </body>
