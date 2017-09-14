0a1510e42f8c4e689c3e1c0d59674d30317bab31
Willian Moro Conk <willmgrfu@gmail.com>
2017-09-14 00:04:51 -0300

    Devmind commit in 2017-09-14 00:06:34

--- porcoweb.php
+++ porcoweb.php
@@ -10,14 +10,9 @@
 <h3>josias</h3>
 <h3>josicreyton</h3>
 <?php
-echo htmlentities("arousen dauzen");
-?>
-<?php
-$name = 'jose' ;
-?>
-<?php 
-$age - 'Vinte e Um' ;
-?>
+echo htmlentities("arousen dauzen"); ?>
+<?php $name = 'jose' ;?>
+<?php $age - 'Vinte e Um' ; ?>
 <?php echo "Talvez . $name é um teste" ; ?>
 
 </body>
