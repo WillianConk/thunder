df52796e7e6fc76ca73a0d2078d0ca4b47d9573a
Willian Moro Conk <willmgrfu@gmail.com>
2017-09-14 00:07:07 -0300

    Devmind commit in 2017-09-14 00:08:50

--- porcoweb.php
+++ porcoweb.php
@@ -3,17 +3,19 @@
 <body>
 
 <h1>porcoweb.caiunanet<h1>
-<h2> Salva sozinho? Não</h2>
+<h2>ainda em construção</h2>
 <h2> php? sim</h2>
 <h3>teste</h3>
 <h3>jose</h3>
 <h3>josias</h3>
 <h3>josicreyton</h3>
+
 <?php
-echo htmlentities("arousen dauzen"); ?>
-<?php $name = 'jose' ;?>
-<?php $age - 'Vinte e Um' ; ?>
-<?php echo "Talvez . $name é um teste" ; ?>
+$name = 'Willian'
+echo htmlentities("arousen dauzen") ;
+echo htmlentities("$name") ;
+?>
+
 
 </body>
 </html>
