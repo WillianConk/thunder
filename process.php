a7876c9729013e520039d386d8e8c1bc6392abcd
Willian Moro Conk <willmgrfu@gmail.com>
2017-09-26 13:39:38 -0300

    Devmind commit in 2017-09-26 13:41:26

---
+++ process.php
@@ -0,0 +1,13 @@
+<html>
+    <head>porta dos desesperados</head>
+<body>
+    
+<?php
+
+    $name = $_POST["eusoucotoco"];
+    echo "ra ra yeah yeah, voce disse:" . $name;
+    
+?>
+    
+</body>    
+</html>
