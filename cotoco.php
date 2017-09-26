b1567606dcc03a473aa6b265cb9fce7546b10bfb
Willian Moro Conk <willmgrfu@gmail.com>
2017-09-26 13:32:38 -0300

    Devmind commit ( renaming file ) in 2017-09-26 13:34:26

---
+++ cotoco.php
@@ -0,0 +1,11 @@
+<html>
+<head><title> faça seu login</title></head>
+<body>
+
+<form action="process.php" method="post">
+    Digite a senha:<input name="eusoucotoco" type="text">
+    <input type="submit">
+</form>
+
+<body>
+</html>
