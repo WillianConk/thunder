dd38ec949e955ad803291b4908d7aa61821894db
Lucas Meine <joaolucas.meine@redes.unb.br>
2017-11-18 15:19:51 -0200

    Devmind commit in 2017-11-18 15:22:03

--- porcoweb.php
+++ porcoweb.php
@@ -1,39 +1,16 @@
-<!DOCTYPE HTML>
-<html>
-<body>
-
-<h1>Pigweb<h1>
-<h2>ainda em construção</h2>
-
-<h3>nomes aleatorios</h3>
-<h3>jose</h3>
-<h3>josias</h3>
-<h3>josicreyton</h3>
-
 <?php
 
-    $number1 = 120;
-    $number2 = 125;
-    $number3 = 130;
-    $sum = $number1 + $number3 + $number2;
-    echo "resultado " . $sum;
-    $loggedin = true;
-    if ($loggedin = true) {
-        echo "welcome";
-    } else {
-        echo "please, get out";
-    }
-    $numbers = array();
-
-    for($i = 9999 ; $i < 10000 ; $i++)
+class Porco
+{
+    
+    
+    public function roncar()
     {
-        $numbers[] = "batato";
+        echo 'roooooooooinc';
     }
     
-    print_r($numbers);
+    
+}
 
 
-?>
-
-</body>
-</html>
+Porco::roncar();
