6b46a345c3ba45972c7de96a76e23255d4a534ea
Lucas Meine <joaolucas.meine@redes.unb.br>
2017-11-18 15:26:27 -0200

    Devmind commit in 2017-11-18 15:28:39

--- porcoweb.php
+++ porcoweb.php
@@ -1,23 +1,3 @@
 <?php
 
-class Site
-{
-    private function montar_link() 
-    {
-        return 'https://www.youtube.com/watch?v=3AEjrz7wF6M';
-    }
-    
-    private function montar_html()
-    {
-        return '<a href="'.$this->montar_link().'"></>';
-    }
-    
-    public function mostrar_html()
-    {
-        echo $this->montar_html();
-    }
-    
-}
-
-
-Site::mostrar_html();
+echo "hahaha";
