1a81db1603c55fda4471012aca6be955932df913
Willian Moro Conk <willmgrfu@gmail.com>
2017-11-19 14:21:30 -0200

    Devmind commit in 2017-11-19 14:23:43

--- process.php
+++ process.php
@@ -1,12 +1,18 @@
 <html>
-    <head>hey! de o fora daqui</head>
+    <head>De o fora daqui</head>
 <body>
     
 <?php
-
+    $i = 1;
+    
     $name = $_POST["nomes"];
     echo "AGORA! " . $name;
     
+    while( $i <= 10) {
+        $value Si * 10;
+        echo "$i times 10 is $value";
+        $i++ ;
+    }
 ?>
     
 </body>    
